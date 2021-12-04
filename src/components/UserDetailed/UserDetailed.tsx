6727211b831197467780a46419cc8ab79ed53129
Juan Carlos <jcvalencialopez@proveedor.teco.com.ar>
2021-12-04 23:28:17 +0000

    modal styles changed

--- src/components/UserDetailed/UserDetailed.tsx
+++ src/components/UserDetailed/UserDetailed.tsx
@@ -1,21 +1,28 @@
-
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
-import './UserDetailed.css'
+import "./UserDetailed.css";
 
 const UserDetailed = () => {
   const location = useLocation();
 
-  const {modalOpen, data} = location.state
-  
-  return (
-    modalOpen?<div className='modal'>
-      <h1>Nombre: {data.name} {data.last_name}</h1>
-      <p>Edad: {data.age}</p>
-      <p>Fecha de Nacimiento: {data.born}</p>
-      <Link to="/" className='back'>Volver</Link>
-    </div>: <></>
-  )
-}
+  const { modalOpen, data } = location.state;
 
-export default UserDetailed
+  return modalOpen ? (
+    <div className="modal-container">
+      <div className="modal">
+        <h1>
+          Nombre: {data.name} {data.last_name}
+        </h1>
+        <p>Edad: {data.age} años.</p>
+        <p>Fecha de Nacimiento: {data.born}.</p>
+          <Link to="/" className="back">
+            Volver
+          </Link>
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
+};
+
+export default UserDetailed;
